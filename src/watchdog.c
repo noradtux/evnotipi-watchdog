c5469c2ebd2d0497016fcf42ee54c2475d7eabce
Malte Schröder <malte@tnxip.de>
2019-07-15 07:13:42 +0200

    pio config

--- src/watchdog.c
+++ src/watchdog.c
@@ -4,12 +4,13 @@
 #include <util/delay.h>
 
 #define LED_PORT  PB3   // Pin 2
-#define DIAMEX    PB1   // Pin 6
+#define POWER     PB1   // Pin 6
 #define RPI_OUT   PB2   // Pin 7
 #define RPI_IN    PB4   // Pin 3
 
 #define LOWER_VOLT   192.3 // 12.74V
 #define HIGHER_VOLT  194.4 // 12.75V
+#define EMERGENCY_SHUTOFF 180.0
 
 #define ON  1
 #define OFF 0
@@ -34,10 +35,10 @@ void initPorts() {
 
    // init outputs
    DDRB |= (1 << LED_PORT);
-   DDRB |= (1 << DIAMEX);
+   DDRB |= (1 << POWER);
    DDRB |= (1 << RPI_OUT);
    DDRB &= ~(1 << RPI_IN);
-   PORTB |= (1 << RPI_IN);   // Enable Pull-Up
+   PORTB &= ~(1 << RPI_IN);   // Disable Pull-Up
 }
 
 float readADC() {
@@ -57,17 +58,6 @@ void ledOff() {
    PORTB &= ~(1 << LED_PORT);
 }
 
-void powerOn() {
-      PORTB |= (1 << DIAMEX);
-}
-void powerOff() {
-      PORTB &= ~(1 << DIAMEX);
-}
-
-uint8_t getPower() {
-   return (PORTB & (1 << DIAMEX)) != 0;
-}
-
 void shutdownEnable() {
    PORTB |= (1 << RPI_OUT);
 }
@@ -75,58 +65,97 @@ void shutdownDisable() {
    PORTB &= ~(1 << RPI_OUT);
 }
 
+void powerOn() {
+   shutdownDisable();
+   PORTB |= (1 << POWER);
+}
+void powerOff() {
+   shutdownEnable();
+   PORTB &= ~(1 << POWER);
+}
+
+uint8_t getPower() {
+   return (PORTB & (1 << POWER)) != 0;
+}
+
 uint8_t getPiStatus() {
    return (PORTB & (1 << RPI_IN)) == 0;
 }
-
 
 int main(void) {
 
    initPorts();
 
    int8_t state_count = 0;
+   float voltage_fl;
 
    while(1) {
-
-      ledOff();
-      float voltage_fl = readADC();
       ledOn();
+      voltage_fl = readADC();
 
-      if (voltage_fl > HIGHER_VOLT) {
-         if (state_count > 35) {
+      if (voltage_fl <= EMERGENCY_SHUTOFF) {
+         powerOff();
+         _delay_ms(900);
+         ledOff();
+         _delay_ms(100);
+      }
+      else if (voltage_fl > HIGHER_VOLT) {
+         if (state_count > 2) {
             if (getPower() == OFF) {
-               ledOn();
                powerOn();
                _delay_ms(2000);
-               if (getPiStatus() == OFF) {   // RPi didn't boot, reset power
-                  powerOff();
-                  _delay_ms(1000);
-                  powerOn();
-               }
+               ledOff();
+            }
+            else if (getPower() == ON && getPiStatus() == OFF) { // RPi didn't boot, reset power
+               powerOff();
+               _delay_ms(1000);
+               powerOn();
+               _delay_ms(500);
+               ledOff();
+               _delay_ms(500);
             }
             else {
                shutdownDisable();
+               _delay_ms(500);
+               ledOff();
+               _delay_ms(500);
             }
          }
          else {
             if (state_count < 0) state_count = 0;
             state_count++;
+            _delay_ms(100);
+            ledOff();
+            _delay_ms(900);
          }
       }
       else if (voltage_fl <= LOWER_VOLT) {
-      	 if (state_count < -35) {
-            ledOff();
+      	 //if (state_count < -2) {
             if (getPiStatus() == ON) {
                shutdownEnable();
+               _delay_ms(100);
+               ledOff();
+               _delay_ms(100);
+               ledOn();
+               _delay_ms(100);
+               ledOff();
+               _delay_ms(700);
             }
             else {
                powerOff();
+               _delay_ms(100);
+               ledOff();
+               _delay_ms(300);
+               ledOn();
+               _delay_ms(100);
+               ledOff();
+               _delay_ms(500);
             }
-      	 }
-         else {
+      	 //}
+         //else {
             if (state_count > 0) state_count = 0;
-            state_count--;
-      	 }
+         //   state_count--;
+      	 //}
       }
    }
 
